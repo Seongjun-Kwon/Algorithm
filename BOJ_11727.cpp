ecd57a4fc25525f9358250ea4e5cf3a92a8966cf
Seongjun Kwon <82152173+SeoungjunKwon@users.noreply.github.com>
2021-06-27 16:48:49 +0900

    Create BOJ_11727.cpp

---
+++ BOJ_11727.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n;
+int D[1005];
+
+int main(void)
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+	D[1] = 1;
+	D[2] = 3;
+
+	for (int i = 3; i <= n; i++)
+	{
+		D[i] = (D[i - 1] + 2 * D[i - 2]) % 10007;
+	}
+
+	cout << D[n];
+}
