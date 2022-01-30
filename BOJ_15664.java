ebc6fa7d60d338a9a318faec7b13a1a2eeab4153
Seongjun Kwon <82152173+Seongjun-Kwon@users.noreply.github.com>
2022-01-30 22:46:33 +0900

    Create BOJ_15664.java

---
+++ BOJ_15664.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static StringBuilder sb;
+    static int n, m;
+    static int[] input;
+    static int[] ans;
+    static boolean[] vis;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        input = new int[n];
+        ans = new int[m];
+        vis = new boolean[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            input[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(input);
+
+        solve(0, 0);
+
+        bw.write(sb.toString());
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    static void solve(int idx, int cnt) {
+        if (cnt == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(ans[i]).append(' ');
+            }
+            sb.append('\n');
+
+            return;
+        }
+
+        for (int i = idx; i < n; i++) {
+            if (vis[i] || ans[cnt] == input[i]) { // 방문했던 인덱스거나, 현재 ans 자리에 이미 들어갔었던 숫자가 들어온다면 continue
+                continue;
+            }
+
+            vis[i] = true;
+            ans[cnt] = input[i];
+            solve(i, cnt + 1); // 비내림차순이므로 다음 숫자도 i부터 탐색
+            vis[i] = false;
+        }
+        ans[cnt] = 0;
+    }
+}
