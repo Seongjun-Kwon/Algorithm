72d44e0c4d54d90d72fb412a5ab2c705a0e50b10
Seongjun Kwon <82152173+Seongjun-Kwon@users.noreply.github.com>
2022-09-15 05:35:52 +0900

    Create BOJ_1344.java

---
+++ BOJ_1344.java
@@ -0,0 +1,48 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        double A = Double.parseDouble(br.readLine()) / 100.0;
+        double B = Double.parseDouble(br.readLine()) / 100.0;
+        double[] perA = new double[19]; // perA[i] = A팀이 i골을 넣을 확률
+        double[] perB = new double[19];
+
+        perA[1] = combination(18, 1) * Math.pow(A, 1) * Math.pow((1.0 - A), 17);
+        perB[1] = combination(18, 1) * Math.pow(B, 1) * Math.pow((1.0 - B), 17);
+
+        for (int i = 2; i < 19; i++) {
+            if (A == 1.0) {
+                perA[i] = 0;
+            } else {
+                perA[i] = perA[i - 1] / combination(18, i - 1) * combination(18, i) / (1.0 - A) * A;
+            }
+
+            if (B == 1.0) {
+                perB[i] = 0;
+            } else {
+                perB[i] = perB[i - 1] / combination(18, i - 1) * combination(18, i) / (1.0 - B) * B;
+            }
+        }
+
+        double primePerA = perA[2] + perA[3] + perA[5] + perA[7] + perA[11] + perA[13] + perA[17];
+        double primePerB = perB[2] + perB[3] + perB[5] + perB[7] + perB[11] + perB[13] + perB[17];
+
+        double answer = primePerA + primePerB - primePerA * primePerB;
+
+        bw.write(Double.toString(answer));
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    static int combination(int n, int r) {
+        if (r == 0 || r == n) {
+            return 1;
+        }
+
+        return combination(n - 1, r - 1) + combination(n - 1, r);
+    }
+}
